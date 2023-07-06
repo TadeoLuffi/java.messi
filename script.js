5af9a82cbe4303ea9d68f0abe50039159e166346
TadeoLuffi <131204961+TadeoLuffi@users.noreply.github.com>
2023-07-06 17:40:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function girar(){
+    alert ("Hola soy Tadeo")
+
+    document.querySelector(".girar").classList.toggle("loop")
+}
+function generarmessi(){
+document.querySelector(".contenedor").innerHTML += "<div class='messi' ></div>"
+}
